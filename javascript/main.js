376f4827af8ae8722b71f22095bb7d67512c4996
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-01-09 16:16:44 -0300

    Estilizando e configurando a parte quadro do projeto, parte tabela de horarios

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
 const scroolLado = document.querySelectorAll(".show-on-scroll");
 const scrollCima = document.querySelectorAll(".show-on-scroll-cima");
-
 const scrollBaixo = document.querySelectorAll(".show-on-scroll-baixo");
-
-console.log(scrollBaixo);
+const seg_Sex = document.getElementById("seg_Sex");
+const sab = document.getElementById("sab");
+const tabela_Horarios = document.getElementById("tabela_Horarios");
+const tabela_Horarios_Sab = document.getElementById("tabela_Horarios_Sab");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -31,4 +32,29 @@ scroolLado.forEach((item) => {
 
 scrollBaixo.forEach((item) => {
   observerTop.observe(item);
+});
+
+sab.addEventListener("click", (e) => {
+  tabela_Horarios.classList.add("clicou");
+  setTimeout(() => {
+    tabela_Horarios.classList.add("display_none");
+  }, 1000);
+
+  setTimeout(() => {
+    tabela_Horarios_Sab.classList.add("display");
+    tabela_Horarios_Sab.classList.remove("display_none");
+    tabela_Horarios_Sab.classList.remove("clicou");
+  }, 1000);
+});
+
+seg_Sex.addEventListener("click", () => {
+  tabela_Horarios_Sab.classList.add("clicou");
+  setTimeout(() => {
+    tabela_Horarios_Sab.classList.add("display_none");
+  }, 1000);
+  setTimeout(() => {
+    tabela_Horarios.classList.add("display");
+    tabela_Horarios.classList.remove("display_none");
+    tabela_Horarios.classList.remove("clicou");
+  }, 1000);
 });
